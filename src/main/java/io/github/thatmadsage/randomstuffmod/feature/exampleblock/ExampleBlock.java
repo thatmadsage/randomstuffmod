a35230396f4b211eb6960bb9d2a84063a7d17177
thatmadsage <226867538+thatmadsage@users.noreply.github.com>
2025-08-17 16:13:17 +0300

    alien (1979)

--- src/main/java/io/github/thatmadsage/randomstuffmod/feature/exampleblock/ExampleBlock.java
+++ src/main/java/io/github/thatmadsage/randomstuffmod/feature/exampleblock/ExampleBlock.java
@@ -2,15 +2,21 @@ package io.github.thatmadsage.randomstuffmod.feature.exampleblock;
 
 import java.util.List;
 
-import io.github.thatmadsage.randomstuffmod.RandomStuffMod;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.Tags.EntityTypes;
 
 public class ExampleBlock extends Block {
 
@@ -36,4 +42,16 @@ public class ExampleBlock extends Block {
         }
     }
     
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if (level.isClientSide()) return;
+        if (entity instanceof Player) return;
+        if (entity instanceof Cat) return;
+        if (!(entity instanceof LivingEntity)) return;
+        if (!entity.isAlive()) return;
+        var entity_pos = entity.position();
+        entity.kill();
+        var cat = EntityType.CAT.spawn((ServerLevel) level, BlockPos.containing(entity_pos), MobSpawnType.CONVERSION);
+        cat.setPos(entity_pos);
+    }
 }
